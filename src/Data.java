1ee442d39bde9cb8e5e40e88b25a83b94a94d8ec
Людок <skobeleva.ludmila@yandex.ru>
2022-12-16 15:42:34 +0300

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    String  login;
+    String password;
+    String confirmPassword;
+
+
+    public Data(String login, String password, String confirmPassword) {
+        if (login==)
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+}
